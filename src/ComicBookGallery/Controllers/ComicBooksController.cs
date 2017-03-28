8149b11a91c6d30b19433f42e38d6676f07d7361
xtropica <mc-johansen@hotmail.com>
2017-03-28 15:41:22 +0200

    Added Comic Book Detail View

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -9,14 +9,7 @@ namespace ComicBookGallery.Controllers
     public class ComicBooksController : Controller
     {
         public ActionResult Detail() {
-
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
-            {
-                return Redirect("/");
-            }
-
-            return Content("Hello from the comic books controller!");
-            
+            return View;            
         }
     }
 }
